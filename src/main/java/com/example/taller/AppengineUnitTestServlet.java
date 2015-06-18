567466ac49a6b9edf5fe88aa112341c378e50b32
edyluisrey <edy.aguirre@tallertechnologies.com>
2015-06-18 17:23:58 -0500

    basic proyect app engine

---
+++ src/main/java/com/example/taller/AppengineUnitTestServlet.java
@@ -0,0 +1,13 @@
+package com.example.taller;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class AppengineUnitTestServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
