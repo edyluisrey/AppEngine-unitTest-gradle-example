4321c17c5ae085a66b0413da7d7166885c8d2a69
edyluisrey <edy.aguirre@tallertechnologies.com>
2015-06-18 17:45:28 -0500

    add junit 4

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,13 @@
+package com.test.util;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class MyFirstTest {
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+      
+}
